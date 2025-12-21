73836e2d0a7612010ec34853821fba25cef3941a
HakimRashid <rashidhakim514@gmail.com>
2025-12-21 02:07:20 -0800

    Added a fully functional navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
+import NavigationBar from "./components/NavigationBar.tsx";
+import About from "./pages/About.tsx";
+import Projects from "./pages/Projects.tsx";
+import Description from "./pages/Description.tsx";
+import React from "react";
 
 function App() {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (pathname === "/") {
+      navigate("/about");
+    }
+  });
+
   return (
-    <Flex w="100vw" h="100vh" alignItems={"center"} justifyContent={"center"}>
-      <Text fontWeight={"bold"}>Coming Soon!</Text>
-    </Flex>
+    <NavigationBar>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/resume" element={<Projects />} />
+        <Route path="/description" element={<Description />} />
+      </Routes>
+    </NavigationBar>
   );
 }
 
