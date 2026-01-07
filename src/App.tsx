9b50a166111926765245ca3e2a4e2ad6dcc40df1
Hakim Rashid <161984674+HakimRashid@users.noreply.github.com>
2026-01-07 11:37:09 -0800

    Revert "Added a fully functional navigation bar"

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,10 @@
-import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
-import NavigationBar from "./components/NavigationBar.tsx";
-import About from "./pages/About.tsx";
-import Projects from "./pages/Projects.tsx";
-import Description from "./pages/Description.tsx";
-import React from "react";
+import { Flex, Text } from "@chakra-ui/react";
 
 function App() {
-  const { pathname } = useLocation();
-  const navigate = useNavigate();
-
-  React.useEffect(() => {
-    if (pathname === "/") {
-      navigate("/about");
-    }
-  });
-
   return (
-    <NavigationBar>
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/resume" element={<Projects />} />
-        <Route path="/description" element={<Description />} />
-      </Routes>
-    </NavigationBar>
+    <Flex w="100vw" h="100vh" alignItems={"center"} justifyContent={"center"}>
+      <Text fontWeight={"bold"}>Coming Soon!</Text>
+    </Flex>
   );
 }
 
--- src/pages/Description.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Description = () => {
-  return <div>Coming soon</div>;
-};
-
-export default Description;
